a43feee50c27739c9761f83d36abd010f3d16b64
gekas145 <56126336+gekas145@users.noreply.github.com>
2022-11-20 10:59:00 +0100

    Revert "Rubbish"
    This reverts commit da329d92ae476b15fb1a963b84a683a173cd4fb2.

--- GeneticAlgorithm.py
+++
@@ -1,6 +0,0 @@
-from OptimisationAlgorithm import OptimisationAlgorithm
-
-class GeneticAlgorithm(OptimisationAlgorithm):
-    def __init__(self, iterations, simulation_length, **kwargs):
-        super().__init__(iterations, simulation_length)
-        
